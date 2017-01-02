a82c146c2428a0cd531969efa746f444f5920e3e
angularist <gendash@gmail.com>
2017-01-02 21:40:35 +0200

    Switched to bower for managing client packages. Created an items-list component. These will contain lists of items.

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -4,10 +4,12 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { ItemsListComponent } from './items-list/items-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ItemsListComponent
   ],
   imports: [
     BrowserModule,
