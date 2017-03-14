46200b7acb0ab58c215ce1a0e96621b2a15baf14
grejerdev <gregory.je@gmail.com>
2017-03-14 08:43:09 +0200

    backup

--- app.js
+++ app.js
@@ -2,8 +2,11 @@ var express = require('express');
 var app = express();
 var mongoose = require('mongoose');
 var config = require('./config');
-var setupController = require('./controllers/setupController');
-var apiController = require('./controllers/apiController');
+//var setupController = require('./controllers/setupController');
+var apiGroupController = require('./controllers/apiGroupController');
+var apiItemController = require('./controllers/apiItemController');
+var apiListController = require('./controllers/apiListController');
+
 
 var port = process.env.PORT || 3000;
 
@@ -11,8 +14,10 @@ app.use('/assets', express.static(__dirname + '/public'));
 
 app.set('view engine', 'ejs');
 
-//mongoose.connect(config.getDbConnectionString());
-setupController(app);
-apiController(app);
+mongoose.connect(config.getDbConnectionString());
+//setupController(app);
+apiGroupController(app);
+apiItemController(app);
+apiListController(app);
 
 app.listen(port);
