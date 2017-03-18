0e13489b41274835a655004b7b6090b96c64a115
grejerdev <gregory.je@gmail.com>
2017-03-19 00:38:43 +0200

    fix typo error

--- config/index.js
+++ config/index.js
@@ -1,9 +1,13 @@
 var configValues = require('./config');
 
 module.exports = {
-    
+ 
     getDbConnectionString: function() {
-        return 'YOUR_MONGO_URL';
+        var userAndPassword = configValues.uname+':'+configValues.pwd;
+        console.log('user and password --'+userAndPassword);
+        var connectionString = 'mongodb://'+userAndPassword+'@ds117899.mlab.com:17899/buy_list'
+        console.log(connectionString)
+        return connectionString;
     }
-    
+   
 }
