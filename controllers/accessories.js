d7bbbe8cd6d049c27868fcf2fbe07ef9966a1563
GreJerdev <gregory.je@gmail.com>
2026-01-17 21:40:30 +0200

    remove old files

--- controllers/accessories.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-
-    createResponse: function createResponse(err, errorMessage, data) {
-        var response = {'error':err,'errorMessge':errorMessage,'data':data};
-        return response;
-
-    }
-}
